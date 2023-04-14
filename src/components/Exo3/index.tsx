f18863d5a23f16f10b2f68fe2c7e70e510f229b5
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-04-14 16:19:20 +0200

    exo3

--- src/components/Exo3/index.tsx
+++ src/components/Exo3/index.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './styles.scss';
 
 function Exo3() {
+  const [isNight, setIsNight] = useState(false);
+
   return (
-    <div className="exercice">
+    <div className={isNight ? 'exercice nuit' : 'exercice'}>
       <h2>Exo3 : Lors du click sur le btn nuit, bg de l'exo3 en black et text en white : </h2>
-      <button type="button" className="btn">Jour</button>
-      <button type="button" className="btn">Nuit</button>
+      <button type="button" className="btn" onClick={() => setIsNight(false)}>Jour</button>
+      <button type="button" className="btn" onClick={() => setIsNight(true)}>Nuit</button>
     </div>
   );
 }
